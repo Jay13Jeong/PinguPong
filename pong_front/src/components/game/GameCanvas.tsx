f9204efcc951503440449f425237441e812a6296
yoouyeon <jyeon.yoon59@gmail.com>
2023-01-18 03:55:16 +0900

    fix : game pos state 관리 로직 수정

--- pong_front/src/components/game/GameCanvas.tsx
+++ pong_front/src/components/game/GameCanvas.tsx
@@ -1,19 +1,13 @@
 import React, {useRef, useState, useEffect} from 'react';
-import {useSetRecoilState, useRecoilValue} from 'recoil';
-import {player1Pos, player2Pos, ballPos, gamePos} from '../../recoil/gamePos'
+import {useRecoilValue} from 'recoil';
+import {gamePos} from '../../recoil/gamePos'
 import { Center, Stack } from '../../styles/Layout';
 
 function GameCanvas(props: any) {
 
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const [ctx, setCtx] = useState<CanvasRenderingContext2D>();
-    const setPlayer1Pos = useSetRecoilState(player1Pos);
-    const setPlayer2Pos = useSetRecoilState(player2Pos);
-    const setBallPos = useSetRecoilState(ballPos);
     const getGamePos = useRecoilValue(gamePos);
-
-    var canvasWidth: number; // FIXME : undefined (업데이트가 안되고 있음)
-    var canvasHeight: number;
 
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -23,37 +17,21 @@ function GameCanvas(props: any) {
                 setCtx(context);
             canvas.width = window.innerWidth / 2;
             canvas.height = window.innerHeight / 2;
-            canvasWidth = canvas.width;
-            canvasHeight = canvas.height;
-            setPlayer1Pos(canvasWidth / 2);
-            setPlayer2Pos(canvasHeight / 2);
-            setBallPos({y: canvas.height / 2, x: canvas.width / 2, dir: 1}); // speed....
         }
     }, []);
 
-    function handleArrowKey(move: number) {
-        // console.log(getGamePos.player1);
-        const currentPlayer1Pos = getGamePos.player1 + move;
-        setPlayer1Pos(currentPlayer1Pos); // FIXME : canvas 범위 확인을 위해 아래 주석 조건문 안으로 들어가야 함...
-        // if (currentPlayer1Pos >= 0 && currentPlayer1Pos <= canvasHeight) {
-        //     console.log(currentPlayer1Pos);
-        //     setPlayer1Pos(currentPlayer1Pos);
-        // }
-    }
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        if (canvas) {
+            canvas.focus();
+        }
+    })
 
-    function canvasKeyHandler(e: any) {
-        if (e.key === "ArrowUp")
-            handleArrowKey(1);
-        else if (e.key === "ArrowDown")
-            handleArrowKey(-1);
-    }
-
-    // FIXME : canvas에 자동 포커스 
     return (
         <Center>
             <Stack>
             {getGamePos.player1 ? <div>{getGamePos.player1}</div> : <div>NULL</div>}
-            <canvas id="gameCanvas" style={{background: "grey"}} ref={canvasRef} onKeyDown={canvasKeyHandler} tabIndex={0}>
+            <canvas id="gameCanvas" style={{background: "grey"}} ref={canvasRef} onKeyDown={props.keyHander ? props.keyHander : null} tabIndex={0}>
             </canvas>
             </Stack>
         </Center>
--- pong_front/src/components/game/GameRoom.tsx
+++ pong_front/src/components/game/GameRoom.tsx
@@ -1,16 +1,64 @@
 import React, {useEffect} from "react";
 import GameCanvas from "./GameCanvas";
 import {Center, Stack} from "../../styles/Layout";
-
+import {useSetRecoilState} from 'recoil';
+import {gamePos} from '../../recoil/gamePos'
 
 function GameRoom(props: any) {
+
+    const setGamePosState = useSetRecoilState(gamePos);
+    let canvasWidth: number;
+    let canvasHeight: number;
+
+    let player1Pos: number = 0;
+    let player2Pos: number = 0;
+    let ballPosY: number = 0;
+    let ballPosX: number = 0;
+    let ballDir: number = 0;
+
+    function setGamePos(p1: number, p2: number, y: number, x: number, dir: number) {
+        // 로컬 변수 업데이트
+        player1Pos = p1;
+        player2Pos = p2;
+        ballPosY = y;
+        ballPosX = x;
+        ballDir = dir;
+        // recoil state 업데이트
+        setGamePosState({player1: player1Pos, player2: player2Pos, ball: {y: ballPosY, x: ballPosX, dir: ballDir}});
+        // emit
+    }
+
+    useEffect(() => {
+        // FIXME : canvasWidth, canvasHeight 값이 유지되지 않음
+        // Window 사이즈에 따른 캔버스 크기 설정
+        canvasWidth = window.innerWidth / 2;
+        canvasHeight = window.innerHeight / 2;
+    }, []);
+
+    function handleArrowKey(move: number) {
+        setGamePos(player1Pos + move, player2Pos, ballPosY, ballPosX, ballDir);
+    }
+
+    /**
+     * TODO : 핸들러 위치 고민
+     * 현재는 핸들러를 canvas에 등록하고 있음
+     * 그래서 canvas 밖으로 나가면 핸들러가 작동하지 않음
+     * 이게 맞을까요
+     */
+    function canvasKeyHandler(e: any) {
+        if (e.key === "ArrowUp")
+            handleArrowKey(1);
+        else if (e.key === "ArrowDown")
+            handleArrowKey(-1);
+    }
+
     return (
         <Center>
             <Stack>
                 <div>this is gameroom</div>
                 {/* <GameScore /> */}
                 {/* <GameUsers/> */}
-                <GameCanvas/>
+                <GameCanvas keyHander={canvasKeyHandler}/>
             </Stack>
         </Center>
     );
