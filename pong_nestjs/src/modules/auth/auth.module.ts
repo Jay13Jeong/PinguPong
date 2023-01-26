51174d921d18293c06c8f7d486afb2232d3e3605
Jay13Jeong <38svbe@gmail.com>
2023-01-27 03:57:09 +0900

    test : 2fa test with nodemailer

--- pong_nestjs/src/modules/auth/auth.module.ts
+++ pong_nestjs/src/modules/auth/auth.module.ts
@@ -9,6 +9,9 @@ import { JwtModule } from '@nestjs/jwt';
 import { FtStrategy } from './guard/ft.strategy';
 import { SecondAuthController } from './second-auth/second-auth.controller';
 import { SecondAuthService } from './second-auth/second-auth.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
+import { UsersService } from '../users/users.service';
 
 @Module({
   imports: [
@@ -17,6 +20,29 @@ import { SecondAuthService } from './second-auth/second-auth.service';
     JwtModule.register({
         secret: process.env.JWTKEY,
         signOptions: { expiresIn: "2h" }, //토큰 만료시간 2시간으로 설정.
+    }),
+    MailerModule.forRootAsync({
+        useFactory: () => ({
+              transport: {
+                  host: process.env.HOST_2FA, //'smtp.gmail.com',
+                  port: 465,
+                  ignoreTLS: true,
+                  secure: true,
+                  auth: {
+                      user: process.env.USER_2FA, //'42.4.jjeong@gmail.com',
+                      pass: process.env.PASS_2FA, //앱 비번 16자리.
+                  },
+              },
+              defaults: { from: '"no-reply" <modules@nestjs.com>', },
+              preview: true,
+              template: {
+                dir: __dirname + '/templates',
+                adapter: new PugAdapter(),
+                options: {
+                  strict: true,
+                },
+              },
+        })
     }),
   ],
   providers: [
@@ -28,6 +54,7 @@ import { SecondAuthService } from './second-auth/second-auth.service';
     // JwtStrategy,
     FtStrategy,
     SecondAuthService,
+    UsersService,
   ],
   controllers: [AuthController, SecondAuthController]
 })
--- pong_nestjs/src/modules/auth/second-auth/second-auth.service.ts
+++ pong_nestjs/src/modules/auth/second-auth/second-auth.service.ts
@@ -1,81 +1,21 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { InjectRepository } from '@nestjs/typeorm';
-import { User } from 'src/modules/users/user.entity';
-import { Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class SecondAuthService {
 
-    private code;
+    constructor(private readonly mailerService: MailerService) {}
 
-    // constructor(@InjectRepository(User) private userRepository: Repository<User>, private mailerService: MailerService) {
-    //   this.code = Math.floor(10000 + Math.random() * 90000);
-    // }
-   
-    // async sendConfirmedEmail(user: User) {
-    //   const { email, fullname } = user
-    //   await this.mailerService.sendMail({
-    //     to: email,
-    //     subject: 'Welcome to Nice App! Email Confirmed',
-    //     template: 'confirmed',
-    //     context: {
-    //       fullname,
-    //       email
-    //     },
-    //   });
-    // }
-   
-    // async sendConfirmationEmail(user: any) {
-    //   const { email, fullname } = await user
-    //   await this.mailerService.sendMail({
-    //     to: email,
-    //     subject: 'Welcome to Nice App! Confirm Email',
-    //     template: 'confirm',
-    //     context: {
-    //       fullname,
-    //       code: this.code
-    //     },
-    //   });
-    // }
-
-    // async signup(user: User): Promise<any> {
-    //     try{
-    //        const salt = await bcrypt.genSalt();
-    //        const hash = await bcrypt.hash(user.password, salt);
-    //        const reqBody = {
-    //          fullname: user.fullname,
-    //          email: user.email,
-    //          password: hash,
-    //          authConfirmToken: this.code,
-    //        }
-    //        const newUser = this.userRepository.insert(reqBody);
-    //        await this.sendConfirmationEmail(reqBody);
-    //        return true
-    //     }catch(e){
-    //         return new HttpException(e, HttpStatus.INTERNAL_SERVER_ERROR);
-    //     }
-    // }
-
-    // async signin(user: User, jwt: JwtService): Promise<any> {
-    //     try{
-    //        const foundUser = await this.userRepository.findOne({ email: user.oauthID });
-    //        if (foundUser) {
-    //          if (foundUser.isVerified) {
-    //            if (bcrypt.compare(user.password, foundUser.password)) {
-    //              const payload = { email: user.email };
-    //              return {
-    //                token: jwt.sign(payload),
-    //              };
-    //            }
-    //          } else {
-    //            return new HttpException('Please verify your account', HttpStatus.UNAUTHORIZED)
-    //          }
-    //          return new HttpException('Incorrect username or password', HttpStatus.UNAUTHORIZED)
-    //        }
-    //        return new HttpException('Incorrect username or password', HttpStatus.UNAUTHORIZED)
-    //     }catch(e){
-    //     return new HttpException(e, HttpStatus.INTERNAL_SERVER_ERROR);
-    //     }
-    // }
+    public sendCode(targetEmail: string): void {
+        this.mailerService
+          .sendMail({
+            to: targetEmail, //'test@nestjs.com', // list of receivers
+            from: '"PinguPong" <noreply@pingu.pong.com>', // sender address
+            subject: '[핑구퐁] 2단계인증 코드 ✔', // Subject line
+            text: '2FA', // plaintext body
+            html: 'code : <b>5432</b>', // HTML body content
+          })
+          .then(() => {})
+          .catch(() => {});
+      }
 }
