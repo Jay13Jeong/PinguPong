aa3f4663ec0918eccdb7875749ac02933ac6299d
Yoouyeon <jyeon.yoon59@gmail.com>
2023-01-30 21:42:26 +0900

    feat : profile block/unblock, follow/unfollow button

--- pong_front/src/components/profile/ProfileModal.tsx
+++ pong_front/src/components/profile/ProfileModal.tsx
@@ -5,32 +5,49 @@ import GameRecordList from "../util/card/GameRecordList";
 import { useRecoilState } from "recoil"
 import { profileModalState } from "../../states/recoilModalState";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserPen, faUserPlus } from "@fortawesome/free-solid-svg-icons";
+import { faCircle, faUserPen, faUserPlus, faUserMinus, faUser, faUserSlash } from "@fortawesome/free-solid-svg-icons";
 import "./ProfileModal.scss"
 import * as types from "./User"
 
 /**
- * 
- * @param props.userid : 조회하려고 하는 user id
+ * profileModalState
+ * default: {
+        userId: 0,
+        show: false
+    }
  */
 
 function ProfileModal() {
     const  [showModal, setShowModal] = useRecoilState(profileModalState);
 
+    // const [userInfo, setUserInfo] = useState<types.user>({
+    //     userName: "pinga",
+    //     myProfile: true,    // TODO - 더 좋은 방법이 있을지 생각해보기
+    //     userStatus: "on",
+    //     rank: 0,
+    //     odds: 0,
+    //     record: [
+    //         {idx: 1, p1: "cheolee", p2: "jjeong", p1Score: 10, p2Score: 2},
+    //         {idx: 2, p1: "cheolee", p2: "jjeong", p1Score: 10, p2Score: 1},
+    //         {idx: 3, p1: "cheolee", p2: "jjeong", p1Score: 10, p2Score: 10},
+    //         {idx: 4, p1: "cheolee", p2: "jjeong", p1Score: 1, p2Score: 10},
+    //         {idx: 5, p1: "cheolee", p2: "jjeong", p1Score: 10, p2Score: 10},
+    //         {idx: 6, p1: "jeyoon", p2: "jjeong", p1Score: 10, p2Score: 5}
+    //     ]
+    // });    // 유저 정보
+
     const [userInfo, setUserInfo] = useState<types.user>({
-        userName: "pinga",
-        myProfile: true,    // TODO - 더 좋은 방법이 있을지 생각해보기
-        userStatus: "on",
+        userName: "pingi",
+        myProfile: false,    // TODO - 더 좋은 방법이 있을지 생각해보기
+        userStatus: "game",
         rank: 0,
         odds: 0,
         record: [
             {idx: 1, p1: "cheolee", p2: "jjeong", p1Score: 10, p2Score: 2},
-            {idx: 2, p1: "cheolee", p2: "jjeong", p1Score: 10, p2Score: 1},
-            {idx: 3, p1: "cheolee", p2: "jjeong", p1Score: 10, p2Score: 10},
-            {idx: 4, p1: "cheolee", p2: "jjeong", p1Score: 1, p2Score: 10},
-            {idx: 5, p1: "cheolee", p2: "jjeong", p1Score: 10, p2Score: 10},
-            {idx: 6, p1: "jeyoon", p2: "jjeong", p1Score: 10, p2Score: 5}
-        ]
+            {idx: 2, p1: "cheolee", p2: "jjeong", p1Score: 10, p2Score: 1}
+        ],
+        following: false,
+        block: false,
     });    // 유저 정보
 
     useEffect(() => {
@@ -43,13 +60,13 @@ function ProfileModal() {
             case "on":
                 return (
                     <div className="profile-status">
-                        online
+                        <FontAwesomeIcon style={{color: "#00BDAA"}} icon={faCircle}/> Online
                     </div>
                 );
             case "off":
                 return (
                     <div className="profile-status">
-                        offline
+                        <FontAwesomeIcon style={{color: "#FE346E"}} icon={faCircle}/> Offline
                     </div>
                 );
             case "game":
@@ -57,13 +74,45 @@ function ProfileModal() {
                     // TODO 게임으로 가야 함.
                     // TODO 게임중인 본인이 게임 화면으로 이동하는 경우 문제 발생할 수 있음...
                     <button className="profile-status">
-                        game
+                        <FontAwesomeIcon style={{color: "#400082"}} icon={faCircle}/> In Game
                     </button>
                 );
             default:
                 return (null);
         }
     }
+
+    function profileButton () {
+        if (userInfo.myProfile) {
+            return (
+                <div className="profile-button-wrapper">
+                    <button className="profile-button">
+                        <FontAwesomeIcon icon={faUserPen}/> Edit Profile
+                    </button>
+                </div>
+            )
+        }
+        return (
+            <div className="profile-button-wrapper">
+                {userInfo.following ? 
+                <button className="profile-button">
+                    <FontAwesomeIcon icon={faUserMinus}/> Unfollow
+                </button> :
+                <button className="profile-button">
+                    <FontAwesomeIcon icon={faUserPlus}/> Follow
+                </button>}
+                {userInfo.block ? 
+                <button className="profile-button">
+                    <FontAwesomeIcon icon={faUser}/> Unblock
+                </button> :
+                <button className="profile-button">
+                    <FontAwesomeIcon icon={faUserSlash}/> Block
+                </button>}
+            </div>
+        )
+    }
+
+    
 
     if (showModal.show) {
         return (
@@ -73,15 +122,11 @@ function ProfileModal() {
                     <div className="profile-box">
                         <img className="profile-image" src="https://cdn.myanimelist.net/images/characters/11/421848.jpg" alt="{userInfo.userName}-profile" />
                     </div>
-                    {userInfo.myProfile ? 
-                        <button className="profile-button">
-                            <FontAwesomeIcon icon={faUserPen}/> Edit Profile</button> : 
-                        <button className="profile-button">
-                            <FontAwesomeIcon icon={faUserPlus}/> Follow</button>}
+                    {profileButton()}
                     <div className="profile-name">
                         ID : {userInfo.userName}
                     </div>
-                    {showStatus(userInfo.userStatus)}
+                    {showStatus(userInfo.userStatus)}   
                     <div className="profile-rank">
                         Rank : {userInfo.rank}
                     </div>
