8b44b9b4f330253066604e3ae72e3cfff2ab73b5
Yoouyeon <jyeon.yoon59@gmail.com>
2023-01-31 15:22:41 +0900

    feat : socket context

--- pong_front/src/components/game/MatchMake.tsx
+++ pong_front/src/components/game/MatchMake.tsx
@@ -1,40 +1,44 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useContext} from "react";
 import { useNavigate } from "react-router-dom";
-// import { socket } from "../../App";
+import { SocketContext } from "../../states/contextSocket";
 import { Center, Stack } from "../../styles/Layout";
 import { Button } from "../../styles/Inputs";
 import DifficultyButtons from "./DifficultyButtons";
 import Loader from "../util/Loader";
-
-/**
- * NOTE - Socket.io Event
- * - REQUEST_MATCH_MAKE : 매치를 요청함
- * - MATCH_MAKE_SUCCESS : 매치가 성공적으로 이루어짐 (방 이동)
- * - MATCH_MAKE_FAIL : 매치가 실패함 (에러) ? 필요한가?
- */
 
 function MatchMake(props: any) {
     const [loading, setLoading] = useState<boolean>(false);
     var currentDifficulty: number = 0;
     const navigate = useNavigate();
 
+    /* socket */
+    const socket = useContext(SocketContext);
+
     useEffect(() => {
-        // socket.on('MATCH_MAKE_SUCCESS', (data: any) => {
-        //     console.log('MATCH_MAKE_SUCCESS');
-        //     // TODO 방 번호 뿐 아니라 플레이어들의 정보도 받아야 함. (상대의 id도 표시해줘야함)
-        //     navigate(`/game/match/${data.room}`, {state: {
-        //         player1: data.p1,
-        //         player2: data.p2
-        //     }});
-        // });
+        socket.on('matchMakeSuccess', (data: any) => {
+            console.log('matchMakeSuccess');
+            /**
+             * NOTE
+             * /game/match/뒤는 그냥 방을 구분할 수 있는 어떤 것이어도 됩니다.
+             * 지금은 (p1닉네임)vs(p2닉네임
+             */
+            navigate(`/game/match/${data.p1}vs${data.p2}`, {state: {
+                player1: data.p1,
+                player2: data.p2
+            }});
+        });
     }, []);
 
+    // NOTE - 매치 메이킹
     function handleMatchMakeRequest(e: any) {
-        // socket.emit('REQUEST_MATCH_MAKE', {difficulty: currentDifficulty});
+        socket.emit('requestMatchMake', {
+            difficulty: currentDifficulty,
+            player: "pingpong_king" // TODO - 본인 아이디 받아와야 함.
+        });
         setLoading(true);
     }
 
-    // TODO - test button용 핸들러
+    // NOTE - test button용 핸들러 (매칭 이후 단순 플레이만 테스트 할 수 있음.)
     function handleTest(e: any) {
         navigate(`/game/match/hh`, {state: {
             player1: "pingpong_king",
