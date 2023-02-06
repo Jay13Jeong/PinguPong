69302d7fc039cac3d5703739e38ea4d4f2c6ba50
Yoouyeon <jyeon.yoon59@gmail.com>
2023-02-06 15:02:29 +0900

    feat : 관전 종료 (정상 종료 / 중간 종료) 처리

--- pong_front/src/components/game/GameWatchRoom.tsx
+++ pong_front/src/components/game/GameWatchRoom.tsx
@@ -1,34 +1,30 @@
-import React, {useEffect, useContext} from "react";
-import {useLocation, useNavigate} from "react-router-dom";
+import React, {useState, useEffect, useContext} from "react";
+import {useLocation, useNavigate, Link} from "react-router-dom";
 import {useSetRecoilState} from "recoil";
 import {gameState} from "../../states/recoilGameState"
 import { Center, Stack } from "../../styles/Layout";
 import { Button } from "../../styles/Inputs";
 import GameRoom from "./GameRoom";
 import { SocketContext } from "../../states/contextSocket";
+import { OverLay, Wrapper } from "../../styles/Modal";
 import * as types from "./Game";
 
-function GameWatchRoom(props: any) {
-    /* 유저 정보 변수들 */
+function GameWatchRoom() {
     const location = useLocation();
     const navigate = useNavigate();
-
-    /* 소켓 */
     const socket = useContext(SocketContext);
+    const setGame = useSetRecoilState<types.gamePosInfo>(gameState);
+    const [winner, setWinner] = useState<string>();
 
     const player1 = location.state.player1;
     const player2 = location.state.player2;
-
-    /* 게임 정보 setter */
-    const setGame = useSetRecoilState<types.gamePosInfo>(gameState);
 
     useEffect(() => {
         socket.on("ballPos", (data: types.gamePosInfo) => {
             setGame(data);
         })
-        socket.on("endGame", (data) => {
-            // TODO - 게임 종료 이벤트 발생시 종료 화면 띄우기
-            console.log(data);
+        socket.on("endGame", (data: {winner: string}) => {
+            setWinner(data.winner);
         })
         return (() => {
             socket.off("ballPos");
@@ -49,6 +45,13 @@ function GameWatchRoom(props: any) {
                     관전 종료
                 </Button>
             </Stack>
+            {winner ? 
+            <OverLay z_index={100}>
+                <Wrapper>
+                <div>Winner is {winner}!</div>
+                <Link to="/lobby"><Button>Go To Lobby</Button></Link>
+            </Wrapper>
+        </OverLay> : null}
         </Center>
     );
 }
