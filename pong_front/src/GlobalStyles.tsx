7d791eeff1b88877d90f9b8e7b4b2f2d691de918
yoouyeon <jyeon.yoon59@gmail.com>
2023-02-11 15:48:01 +0900

    feat : 모든 페이지에 랜덤 배경 설정

--- pong_front/src/GlobalStyles.tsx
+++ pong_front/src/GlobalStyles.tsx
@@ -1,11 +1,24 @@
 import { createGlobalStyle } from "styled-components";
+import { pingu1, pingu2, pingu3, pingu4, pingu5, pingu6, pingu7, pingu8 } from "./assets/background";
+
+function randomNum(){
+    const max = 8; //마지막 이미지 번호.
+    const min = 1; //최초 이미지 번호.
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+const backgrounds = [pingu1, pingu2, pingu3, pingu4, pingu5, pingu6, pingu7, pingu8];
+
+const backgroundImg = backgrounds[randomNum()];
 
 const GlobalStyle = createGlobalStyle`
-    button {
-        background-color: transparent;
-        border: none;
-        outline: none;
-        cursor: pointer;
+    body {
+        background-image: url(${backgroundImg});
+        margin:0px;
+        width:100vw;
+        height:calc(100vh - 50px);
+        background-size: cover;
+        background-repeat: no-repeat;
     }
 `;
 
