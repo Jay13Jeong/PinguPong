096ce0ef91bcf56cbc7bb85272651e12aee007e4
FeFe2200 <dlcjf2200@naver.com>
2023-03-09 22:45:22 +0900

    style:사라진 디비타입 제거

--- pong_nestjs/src/app.module.ts
+++ pong_nestjs/src/app.module.ts
@@ -10,7 +10,6 @@ import { Friend } from './modules/friend/friend.entity';
 import { FriendModule } from './modules/friend/friend.module';
 import { Game } from './modules/game/game.entity';
 import { Chat} from './modules/chat/chat.entity';
-import { Message } from './modules/chat/msg.entity';
 import { SocketModule } from './modules/socket/socket.module';
 import { Ban } from './modules/chat/ban.entity';
 import { Mute } from './modules/chat/mute.entity';
@@ -32,7 +31,7 @@ import { DmMsgDb } from './modules/chatdm/dmMsgDb.entity';
       database: process.env.DB_NAME_DEVELOPMENT, // db name.
       autoLoadEntities: true, // 엔티티를 자동으로 로드.
       synchronize: true, // 앱을 실행할 때마다 운영 주체가 데이터베이스와 동기화. 개발모드에서만 써야함.
-      entities: [Users, Friend, Game, Chat, Message, Ban, Mute, RoomUserId, DmList, DmMsgDb] //디비가 다룰 엔티티목록.
+      entities: [Users, Friend, Game, Chat, Ban, Mute, RoomUserId, DmList, DmMsgDb] //디비가 다룰 엔티티목록.
     }),
     UsersModule,
     AuthModule,
