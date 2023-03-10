8ee51981080a7f6418297ff45291c2beba522ce7
Jaegu Jeong <jjeong@c2r10s7.42seoul.kr>
2023-03-10 15:52:06 +0900

    fix : invalid api request

--- pong_front/src/components/chat/modal/ChatMenuModal.tsx
+++ pong_front/src/components/chat/modal/ChatMenuModal.tsx
@@ -17,7 +17,7 @@ function ChatMenuModal (props: {roomName: string, isMaster: boolean, setMaster?:
     const modalState = useRecoilValue(chatMenuModalState);
     const resetState = useResetRecoilState(chatMenuModalState);
     const profileState = useSetRecoilState(profileModalState);
-    const [info, error, loading] = useGetData(`http://` + REACT_APP_HOST + `:3000/api/user/name?username=${modalState.user}`, modalState.user);
+    const [info, error, loading] = useGetData(`http://` + REACT_APP_HOST + `/api/user/name?username=${modalState.user}`, modalState.user);
     const [current, setCurrent] = useState("");
     const [myInfo, myerror, myLoading] = useGetData('http://' + REACT_APP_HOST + '/api/user');
     const [menuLoading, isMenuLoading] = useState<boolean>(true);
