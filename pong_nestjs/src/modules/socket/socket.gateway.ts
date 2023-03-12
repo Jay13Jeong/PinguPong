28b8ef64864e26a104be888cbee080b345653f9d
fefe2200 <dlcjf2200@naver.com>
2023-03-13 08:18:27 +0900

    refactor:소켓 관리 레디스 연동

--- pong_nestjs/src/modules/socket/socket.gateway.ts
+++ pong_nestjs/src/modules/socket/socket.gateway.ts
@@ -1,6 +1,6 @@
 import { WebSocketGateway, WebSocketServer, SubscribeMessage, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
-import { Inject } from '@nestjs/common';
+import { CACHE_MANAGER, Inject } from '@nestjs/common';
 import { AuthService } from '../auth/auth.service';
 import { UsersService } from '../users/users.service';
 import { Users } from '../users/user.entity';
@@ -10,6 +10,7 @@ import { Friend } from '../friend/friend.entity';
 import { status } from './userStatusType';
 import { ChatService } from '../chat/chat.service';
 import { ChatDmService } from '../chatdm/chatdm.service';
+import { Cache } from 'cache-manager';
 
 @WebSocketGateway( {
     cors: { origin: '*' }//, credentials: true,}//, namespace: 'api/ping'
@@ -20,52 +21,72 @@ import { ChatDmService } from '../chatdm/chatdm.service';
 		private readonly userService: UsersService,
     private readonly friendService: FriendService,
     private readonly chatService: ChatService,
-    private readonly chatDmService: ChatDmService,)
+    private readonly chatDmService: ChatDmService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,)
     {
     }
 
     @WebSocketServer()
     server: Server;
 
-    socketUserid : Map<string, number> = new Map<string, number>();
-    useridStatus : Map<number, status> = new Map<number, status>();
-
-
     //OnGatewayConnection를 오버라이딩
     async handleConnection(client : Socket) {
       const user = await this.findUserBySocket(client);
 
       if (user != undefined) {//해당 사용자가 디비에 있으면 등록
-        this.socketUserid.set(client.id, user.id);//소켓id와 유저 id 맵핑
+        await this.setUseridStatus(1, user.id);
+        await this.setUseridBySocketid(client.id, user.id);//소켓id와 유저 id 맵핑
         this.chatService.socketSave(user.id, client.id);//소켓통신을 하고 있는 채팅이용자 및 예정자들
-        if (this.useridStatus.has(user.id) === true)//이미 등록된 유저이고 다른 브라우저에서 추가 접속
-          this.useridStatus.get(user.id).count++;
-        else{
-          this.useridStatus.set(user.id, {status:'online', count:1});//유저가 첫번째 브라우저로 들어옴
-        }
       }
     }
 
     //OnGatewayDisconnect를 오버라이딩
     async handleDisconnect(client : Socket) {
       //게임 중인지 파악하고 패배시키기
-      this.gameService.iGamegetout(client, this.socketUserid);// 핑퐁
+      this.gameService.iGamegetout(client);// 핑퐁
 
-      let userId = this.socketUserid.get(client.id);
-      if (userId !== undefined){
-        this.socketUserid.delete(client.id);//해당 소켓id 지우기
+      let userId = await this.getUseridBySocketid(client.id);
+      if (userId !== null){
         this.chatService.socketDelete(userId, client.id);//소켓통신이 끊긴 채팅이용자 및 예정자들;
-        this.useridStatus.get(userId).count--;//해당 유저의 브라우저 접속 카운트 -1
-        if (this.useridStatus.get(userId).count <= 0)//0보다 작으면 맵에서 지우기
-          this.useridStatus.delete(userId);
+        this.setUseridStatus(0, userId);
         this.gameService.delNoGamegetoutSocketList(client.id);//게임중일때 나가면 게임중인 소켓id값에서 지우기
       }
     }
 
-    private changeUseridStatus (userId:number, status: string) {//유저의 현재 위치를 변경하는 함수
-      if (this.useridStatus.has(userId))
-        this.useridStatus.get(userId).status = status;
+    private async changeUseridStatus(userid:number, status: string) {//유저의 현재 위치를 변경하는 함수
+      if (userid == 0)
+        return;
+      let userStatus:status = await this.cacheManager.get<status>(String(userid));
+      userStatus.status = status;
+
+      await this.cacheManager.set(String(userid), userStatus);
           //상태는 offline, online, ingame, matching, 채팅이나 디엠 중이면 해당 룸 네임 넣기
+    }
+
+    private async setUseridStatus(key:number, userid:number){
+      let userStatus:status = await this.cacheManager.get<status>(String(userid));
+
+      if (userStatus === null)
+        userStatus = {status:'online', count:0};
+
+      if (key === 1){//카운트 증가,최초 소켓 연결시
+          userStatus.count++;//유저가 첫번째 브라우저로 들어옴
+      }
+      else if (key === 0){//카운트 감소, 소켓 연결 끊길 때
+        userStatus.count--;
+        if (userStatus.count <= 0)//0보다 작으면 오프라인
+          userStatus.status = 'offline';
+      }
+      await this.cacheManager.set(String(userid), userStatus);
+    }
+
+    private async getUseridBySocketid(socketid:string):Promise<number>{
+      const userid = await this.cacheManager.get<number>(socketid);
+      return userid;
+    }
+
+    private async setUseridBySocketid(socketid:string, userid:number){
+      await this.cacheManager.set(socketid, userid);
     }
 
     //비정제 쿠키 데이터를 파싱하는 메소드.
@@ -92,22 +113,21 @@ import { ChatDmService } from '../chatdm/chatdm.service';
 
   @SubscribeMessage('setInLobby')//사용자가 로비에 들어오면,
   async setInLobby(client : Socket) {
-    let userId:number = this.socketUserid.get(client.id);
+    let userId:number = await this.getUseridBySocketid(client.id);
 
-    this.changeUseridStatus(userId, 'online');
+    await this.changeUseridStatus(userId, 'online');
     //게임 안에 가서 클래스 및 매칭 큐 삭제하기 할 것
-    this.gameService.iGamegetout(client, this.socketUserid);//게임 중에 로비로 올 수 있으니 관련 사항 처리
+    this.gameService.iGamegetout(client);//게임 중에 로비로 올 수 있으니 관련 사항 처리
   }
 
   @SubscribeMessage('getUserStatus')//api/get/user/status
   async getUserStatus(client : Socket, data) {
     let targetId: number = data;
-    let status:string;
+    if (targetId == 0)
+      return;
+    let targetStatus:status = await this.cacheManager.get<status>(String(targetId));
+    let status:string = targetStatus.status;
 
-    if (this.useridStatus.has(targetId)==true)
-      status = this.useridStatus.get(targetId).status;
-    else
-      status = 'offline';
     if (status != 'offline' && status != 'online' && status != 'ingame' && status != 'matching')
       status = 'online';
     this.server.to(client.id).emit('getUserStatus', status, targetId);
@@ -117,7 +137,7 @@ import { ChatDmService } from '../chatdm/chatdm.service';
   @SubscribeMessage('chatDelUser')//방에서 나가기 누르면 해당 방에서 유저 삭제/delUser
   async delUser(client : Socket, data) {
     let roomName:string = data;
-    let userId:number = this.socketUserid.get(client.id);
+    let userId:number = await this.getUseridBySocketid(client.id);
 
     await this.chatService.delUser(roomName, userId, this.server);
   }
@@ -125,7 +145,7 @@ import { ChatDmService } from '../chatdm/chatdm.service';
   @SubscribeMessage('chatGetUser')//해당 유저 등록하기//getUser
   async getUser(client : Socket, data) {
     let room:string = data.roomName;
-    let userId:number = this.socketUserid.get(client.id);
+    let userId:number = await this.getUseridBySocketid(client.id)
     if (await this.chatService.roomCheck(room) == false) {//없는 방에 입장을 하려고 할때
       this.server.to(client.id).emit('notRoom');
       return ;
@@ -136,13 +156,13 @@ import { ChatDmService } from '../chatdm/chatdm.service';
     }
     this.server.to(client.id).emit('youPass');//접속 가능하면
     await this.chatService.newRoom(room, userId);//채팅방에 유저 등록
-    this.changeUseridStatus(userId, room);//유저 상태 변경
+    await this.changeUseridStatus(userId, room);//유저 상태 변경
   }
 
   @SubscribeMessage('chat')//음소거, 차단 유무까지 확인을 해야한다.
   async onChat(client : Socket, data) {
     let [room, msg] = data;
-    let userId:number = this.socketUserid.get(client.id);
+    let userId:number = await this.getUseridBySocketid(client.id);
     let user:Promise<Users> = this.userService.findUserById(userId);
 
     if (await this.chatService.checkRoomInUser(userId, room) == false)//유저가 방에 있는 인원인지 확인하기
@@ -171,7 +191,7 @@ import { ChatDmService } from '../chatdm/chatdm.service';
   async checksecretPw(client : Socket, data) {
     let roomName:string = data.roomName;
     let secretPW:string = data.secret;
-    let userId:number = this.socketUserid.get(client.id);
+    let userId:number = await this.getUseridBySocketid(client.id);
 
     if (await this.chatService.checksecretPw(roomName, secretPW)){//비밀번호 확인
       await this.chatService.newRoom(roomName, userId);
@@ -183,12 +203,12 @@ import { ChatDmService } from '../chatdm/chatdm.service';
   @SubscribeMessage('chatPostNewRoom')//새로운 방 만들기, 이미 있는 방이름이면 false 반환///api/post/newRoom
   async newRoom(client : Socket, data) {
     let [room, secretpw] = data;
-    let userId:number = this.socketUserid.get(client.id);
+    let userId:number = await this.getUseridBySocketid(client.id);
 
     if (!(await this.chatService.roomCheck(room)) && (room.length <= 10)){//방이름 체크 및 글자수 제한 확인, 한글로 채팅방이름 못 만듬
       await this.chatService.newRoom(room, userId, secretpw);
       this.server.to(client.id).emit('chatPostNewRoom', true);
-      this.changeUseridStatus(userId, room);
+      await this.changeUseridStatus(userId, room);
     }
     else
       this.server.to(client.id).emit('chatPostNewRoom', false);
@@ -197,7 +217,7 @@ import { ChatDmService } from '../chatdm/chatdm.service';
   @SubscribeMessage('chatGetMasterStatus')//내가 마스터 이면 true, 아니면 false///api/get/master/status
   async getMasterStatus(client : Socket, data) {
     let roomName:string = data;
-    let userid = this.socketUserid.get(client.id);
+    let userid:number = await this.getUseridBySocketid(client.id);
 
     this.server.to(client.id).emit('chatGetMasterStatus', await this.chatService.getMasterStatus(roomName, userid));// 클래스 함수 리턴값으로 고치기
   }
@@ -205,14 +225,14 @@ import { ChatDmService } from '../chatdm/chatdm.service';
   @SubscribeMessage('chatGetRoomList')//브라우저가 채팅방 리스트 요청함///api/get/RoomList
   async getChatList(client : Socket) {
     this.server.to(client.id).emit('chatGetRoomList', await this.chatService.getRoomList());// 리스트 보내주기, 클래스 함수 리턴값으로 고치기
-    //this.changeUseridStatus(this.socketUserid.get(client.id), 'online');//이 부분 빼도 되지 않나?
+    //this.changeUseridStatus(await this.getUseridBySocketid(client.id), 'online');//이 부분 빼도 되지 않나?
   }
 
   @SubscribeMessage('chatPostMandateMaster')//방장위임 하기///api/post/mandateMaster
   async mandateMaster(client : Socket, data) {
     let [room, targetId] = data;//위임할 targetId
 
-    let userId:number = this.socketUserid.get(client.id);
+    let userId:number = await this.getUseridBySocketid(client.id);
     await this.chatService.mandateMaster(this.server, room, userId, targetId);
   }
 
@@ -220,7 +240,7 @@ import { ChatDmService } from '../chatdm/chatdm.service';
   async setSecretpw(client : Socket, data){
     let [roomName, newsecret] = data;
 
-    let userId:number = this.socketUserid.get(client.id);
+    let userId:number = await this.getUseridBySocketid(client.id);
     await this.chatService.setSecretpw(roomName, userId, newsecret);
   }
 
@@ -229,7 +249,7 @@ import { ChatDmService } from '../chatdm/chatdm.service';
   async addmuteuser(client : Socket, data) {
     let [roomName, targetId] = data;//음소거할 타겟id
 
-    let userId:number = this.socketUserid.get(client.id);
+    let userId:number = await this.getUseridBySocketid(client.id);
     await this.chatService.addmuteuser(roomName, userId, targetId);
   }
 
@@ -237,7 +257,7 @@ import { ChatDmService } from '../chatdm/chatdm.service';
   async freemuteuser(client : Socket, data) {
     let [room, targetId] = data;//음소거 해제할 타겟id
 
-    let userId:number = this.socketUserid.get(client.id);
+    let userId:number = await this.getUseridBySocketid(client.id);
     await this.chatService.freemuteuser(room, userId, targetId);
   }
 
@@ -250,14 +270,14 @@ import { ChatDmService } from '../chatdm/chatdm.service';
   @SubscribeMessage('kickUser')//넌 킥
   async kickuser(client: Socket, data) {
     let [roomName, targetId] = data;//킥할 타켓 id
-    let userId:number = this.socketUserid.get(client.id);
+    let userId:number = await this.getUseridBySocketid(client.id);
     await this.chatService.kickUser(this.server, roomName, userId, targetId);
   }
 
   @SubscribeMessage('banUser')//너 영구밴
   async banUser(client: Socket, data) {
     let [roomName, targetId] = data;
-    let userId:number = this.socketUserid.get(client.id);
+    let userId:number = await this.getUseridBySocketid(client.id);
     await this.chatService.banUser(this.server, roomName, userId, targetId);
   }
 
@@ -270,7 +290,7 @@ import { ChatDmService } from '../chatdm/chatdm.service';
 
   @SubscribeMessage('dmList')//디엠 기능 첫 입장, 처음이면 DM 디비 만들기
   async dmList(client:Socket){
-    const userId:number = this.socketUserid.get(client.id);
+    const userId:number = await this.getUseridBySocketid(client.id);
 
     let userIds:number[] = await this.chatDmService.getdmList(userId);
     let userName:string[] = [];
@@ -280,7 +300,7 @@ import { ChatDmService } from '../chatdm/chatdm.service';
     }
 
     this.server.to(client.id).emit('dmList', userName);//유저 네임 리스트 보내주기
-    this.changeUseridStatus(userId, 'online');//이거 빼도 되지 않은가?
+    await this.changeUseridStatus(userId, 'online');//이거 빼도 되지 않은가?
   }
 
   //메시지를 보냄
@@ -309,16 +329,16 @@ import { ChatDmService } from '../chatdm/chatdm.service';
     let msgs = await this.chatDmService.getMsgs(user, target);//여태까지 받은 대화들 반환
     this.server.to(client.id).emit('receiveDms', msgs);//반환된 메세지들 보내주기
     //masgs = [{userName : 'tempUser',  msg : '123123123'}, ...]
-    this.changeUseridStatus(user.id, await this.chatDmService.getTargetDmRoom(user.id, targetId));//유저 상태 변경
+    await this.changeUseridStatus(user.id, await this.chatDmService.getTargetDmRoom(user.id, targetId));//유저 상태 변경
   }
 
   //1대1 디엠방 나감
   @SubscribeMessage('closeDm')
   async closeDm(client:Socket, data){
     let targetId:number = data;
-    let userId:number = this.socketUserid.get(client.id);
+    let userId:number = await this.getUseridBySocketid(client.id);
     await this.chatDmService.closeDm(client, userId, targetId);
-    this.changeUseridStatus(userId, 'online');//유저상태 변경
+    await this.changeUseridStatus(userId, 'online');//유저상태 변경
   }
 
 
@@ -340,17 +360,18 @@ import { ChatDmService } from '../chatdm/chatdm.service';
     async requestMatchMake(client : Socket, data) {
        let difficulty:number = data.difficulty;
        let userName:string = data.player;
+       let userid:number = await this.getUseridBySocketid(client.id);
 
-      if (this.gameService.matchCheck(this.socketUserid.get(client.id)) === true){//유저가 이미 매칭중인지 확인
+      if (this.gameService.matchCheck(userid) === true){//유저가 이미 매칭중인지 확인
          this.server.to(client.id).emit('matchFail');//늦게 매칭한 소켓에게 이벤트 전송
         return ;
       }
       // 1. 같은 난이도를 요청한 플레이어가 큐에 있을 경우 게임 매치
       // 2. 같은 난이도를 요청한 플레이어가 큐에 없을 경우 해당 플레이어를 큐에 넣는다.
-       if (this.gameService.matchMake(difficulty, userName, client.id, this.socketUserid.get(client.id), this.server)){
-        this.gameService.matchEmit(this.server, this.socketUserid.get(client.id));
+       if (this.gameService.matchMake(difficulty, userName, client.id, userid, this.server)){
+        this.gameService.matchEmit(this.server, userid);
       }
-      this.changeUseridStatus(this.socketUserid.get(client.id), 'matching');//유저 상태 변경
+      await this.changeUseridStatus(userid, 'matching');//유저 상태 변경
     }
 
     @SubscribeMessage('requestStart')
@@ -360,7 +381,7 @@ import { ChatDmService } from '../chatdm/chatdm.service';
       //플레이어가 준비완료인지 확인하기, 여기서 socket room에 등록을 하자
       if (this.gameService.requestStart(roomName, client, this.server))
         await this.gameService.startGame(roomName, this.server);
-      this.changeUseridStatus(this.socketUserid.get(client.id), 'ingame');
+      await this.changeUseridStatus(await this.getUseridBySocketid(client.id), 'ingame');
         //클래스 안에서 소켓메세지 보내기
         //api: 각각의 방마다 따로 돌아야 한다.
     }
@@ -447,7 +468,7 @@ import { ChatDmService } from '../chatdm/chatdm.service';
         if (roomName === '_dm')
           roomName = await this.chatDmService.getTargetDmRoom(user.id, targetId);
 
-        if (!(this.useridStatus.get(targetId).status === roomName) || (this.gameService.checkGaming(targetId)))
+        if (!((await this.cacheManager.get<status>(String(targetId))).status === roomName) || (this.gameService.checkGaming(targetId)))
           return this.server.to(client.id).emit('duelRequest', false);
 
         let targetSocketIds:Set<string> = this.chatService.getsocketIdByuserId(targetId);
@@ -455,8 +476,8 @@ import { ChatDmService } from '../chatdm/chatdm.service';
         this.gameService.duelRequest(client.id, user, targetSocketId, target, this.server);//방만들기
 
         this.server.to(client.id).emit('duelRequest', true);
-        this.server.to(targetSocketId).emit('duelAccept', this.socketUserid.get(client.id), user.username);
-        this.changeUseridStatus(user.id, 'matching');
+        this.server.to(targetSocketId).emit('duelAccept', await this.getUseridBySocketid(client.id), user.username);
+        await this.changeUseridStatus(user.id, 'matching');
      }
 
 //a가 대기하다 도망가기
@@ -488,16 +509,15 @@ import { ChatDmService } from '../chatdm/chatdm.service';
           return ;
         }
         //대결 상대가 같은 곳에 있는지 확인, 도전장 보낸 사람이 채팅방에서 나간 경우
-        if (this.useridStatus.get(targetId).status != this.useridStatus.get(user.id).status){
+        if ((await this.cacheManager.get<status>(String(targetId))).status != 'matching'){
           let target:Promise<Users> = this.userService.findUserById(targetId);
           this.gameService.duelDelete(user.id, targetId);
           this.server.to(client.id).emit('duelTargetRun', (await target).username);
           return ;
         }
-        //승낙하면 matchSuce
-
+        //승낙하면 matchMakeSuccess
         this.gameService.matchEmit(this.server, user.id);
-        this.changeUseridStatus(user.id, 'matching');
+        await this.changeUseridStatus(user.id, 'matching');
 
         //위의 함수에서 'matchMakeSuccess'이벤트 보냄 이후 게임화면 등장
         //->게임준비 버튼 등 로직은   @SubscribeMessage('requestStart')으로 진행된다.
